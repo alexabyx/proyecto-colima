485cbc550dadbecab65346131a4c00cfc6f5ddbf
Juan Carlos <jcae.escolar@gmail.com>
2015-01-25 22:42:32 -0600

    Commit

--- proyecto_colima/inicio/admin.py
+++ proyecto_colima/inicio/admin.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
-from inicio.models import AreaAdministrativa
-
-# Register your models here.
-
-
-class AreaAdministrativaAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    list_filter = ['nombre']
-    search_fields = ['nombre', 'usuario']
-
-
-
-
-admin.site.register(AreaAdministrativa, AreaAdministrativaAdmin)
-
  

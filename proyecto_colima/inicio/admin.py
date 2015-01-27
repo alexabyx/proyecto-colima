fceed9bc22c895bd2f5b4eebd2e26555f178af31
acruzrojas <acruz_rojas@hotmail.com>
2015-01-27 00:27:08 -0600

    Revert "Settings y tabulars"
    This reverts commit 4044dcd7e2f587fd75d7eca985c1eb712baa30ae.

--- proyecto_colima/inicio/admin.py
+++ proyecto_colima/inicio/admin.py
@@ -32,45 +32,37 @@ class ContratosAdmin(admin.ModelAdmin):
 	list_display = ('numero_oficio', 'proyecto', 'encargado')
 
 
-class DetallesEntregablesInline(admin.TabularInline):
-	model = DetallesEntregables
-	extra = 2
-
 class EntregablesAdmin(admin.ModelAdmin):
 	list_display = ('contrato', 'proyecto', 'responsable')
 	list_filter = ('fecha_creacion',)
 
-	inlines = [DetallesEntregablesInline]
-
+class DetallesEntregablesAdmin(admin.ModelAdmin):
+	list_display = ('entregable', 'responsable','numero')
+	list_filter = ('fecha_creacion',)
 
 class EmpresasAdmin(admin.ModelAdmin):
 	list_display = ('nombre',)
-
-class DetallesFacturasInline(admin.TabularInline):
-	model = DetallesFacturas
-	extra = 2
 
 class FacturasAdmin(admin.ModelAdmin):
 	list_display = ('contrato', 'responsable', 'tipo', 'nombre', 'siglas')
 	list_filter = ('tipo',)
 
-	inlines = [DetallesFacturasInline]
+class DetallesFacturasAdmin(admin.ModelAdmin):
+	list_display = ('factura', 'descripcion', 'cantidad')
 
 class PropuestasAdmin(admin.ModelAdmin):
 	list_display = ('proyecto', 'responsable', 'tipo', 'nombre', 'siglas')
 	list_filter = ('fecha_creacion', 'tipo',)
 
 
-class DetallesDocumentosGeneralesInline(admin.TabularInline):
-	model = DetallesDocumentosGenerales
-	extra = 2
-
 class DocumentosGeneralesAdmin(admin.ModelAdmin):
 	list_display = ('tipo', 'proyecto', 'responsable', 'clave')
 	list_filter = ('fecha_creacion',)
 
-	inlines = [DetallesDocumentosGeneralesInline]
 
+class DetallesDocumentosGeneralesAdmin(admin.ModelAdmin):
+	list_display= ('documentos_generales', 'responsable', 'numero', 'nombre')
+	list_filter=('fecha_creacion',)
 
 admin.site.register(Personal, PersonalAdmin)
 admin.site.register(DetallePagoEmpleado,DetallePagoEmpleadoAdmin)
@@ -80,13 +72,13 @@ admin.site.register(AnexosTecnicos, AnexosTecnicosAdmin)
 admin.site.register(Convenios, ConveniosAdmin)
 admin.site.register(Contratos, ContratosAdmin)
 admin.site.register(Entregables, EntregablesAdmin)
-#admin.site.register(DetallesEntregables, DetallesEntregablesAdmin)
+admin.site.register(DetallesEntregables, DetallesEntregablesAdmin)
 admin.site.register(Empresas, EmpresasAdmin)
 admin.site.register(Facturas, FacturasAdmin)
-#admin.site.register(DetallesFacturas, DetallesFacturasAdmin)
+admin.site.register(DetallesFacturas, DetallesFacturasAdmin)
 admin.site.register(Propuestas, PropuestasAdmin)
 admin.site.register(DocumentosGenerales, DocumentosGeneralesAdmin)
-#admin.site.register(DetallesDocumentosGenerales, DetallesDocumentosGeneralesAdmin)
+admin.site.register(DetallesDocumentosGenerales, DetallesDocumentosGeneralesAdmin)
 
 # class PersonalAdmin(admin.ModelAdmin):
 # 	list_display=('rfc','credencial_elector','nombre','apellido_paterno','apellido_materno','siglas_nombre','genero','direccion','telefono','no_seguro','fecha_ingreso','puesto','turno','tipo_plaza','tipo_pago','monto','numero_oficio_contrato','dias_trabajo_al_mes','fecha_vencimiento_contrato','fecha_baja','motivo_baja','responsable',)
